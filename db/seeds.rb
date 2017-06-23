ad3fd117a95350670729eba529cd4592ba51069f
MarioRoncador <mroncador@alaska.edu>
2017-06-23 15:16:32 -0400

    Database seeds with Faker and tasks are displayed correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+include Faker
+
+# Create Users
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+  )
+end
+User.create!(
+  email: "m@m.com",
+  password: "111111",
+)
+users = User.all
+
+# Create Items
+50.times do
+  Item.create!(
+    name: Faker::Lorem.sentence,
+    user:  users.sample,
+  )
+end
+items = Item.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
